0688ac77c3ea5422e87755c82eaef90550da98f1
Newton Ndumia <newtonndungu242@gmail.com>
2026-05-18 12:04:02 +0300

    fix sitemap for SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://novacore-gamma.vercel.app/";
+  const baseUrl = "https://novacore-gamma.vercel.app";
 
   return [
     {
